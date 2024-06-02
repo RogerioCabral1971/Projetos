ca345e80c855f343677bd060da2a76c4d1f3ab08
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-02 16:31:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ coluna_date1,coluna_date2,coluna_date3,coluna_date4=st.columns(4)
 
 inicial=coluna_date1.date_input('Data Inicio', datetime.date(2024,5,1),format='YYYY-MM-DD')
 fim=coluna_date3.date_input('Data Final', today,format='YYYY-MM-DD')
-st.data_editor(Relatorio.resumo_canal('2024-05-01','2024-05-15', 'Finalizado'))
+st.data_editor(Relatorio.resumo_canal(inicial,fim, 'Finalizado'))
 
 
 
