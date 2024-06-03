6571878907a65bc72d52d5d044c09653d40d802a
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-03 08:01:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ today = datetime.datetime.now()
 coluna_date1,coluna_date2,coluna_date3,coluna_date4=st.columns(4)
 inicial=coluna_date1.date_input('Data Inicio', datetime.date(2024,5,15),format='YYYY-MM-DD')
 fim=coluna_date3.date_input('Data Final', today,format='YYYY-MM-DD')
-st.write('Resumo de total de Vendas - Somente vendas já Faturadas')
+st.title('Pleno Led')
+st.write('Total Venda Bruta - Somente vendas já Faturadas')
 st.data_editor(Relatorio.resumo_canal(inicial,fim, 'Finalizado'))
 
 
