f4edc40aa0a600fbea3326226eae0e57801f1a58
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-16 01:08:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ arq=st.file_uploader("Escolho",accept_multiple_files=True)
 st.write(pd.read_parquet(arq[0]))
 
 
-st.header('Show only molecule files')
-event = st_file_browser("example_artifacts", artifacts_site="http://localhost:1024", show_choose_file=True, show_download_file=False, glob_patterns=('molecule/*',), key='C')
-st.write(event)
 
 
 
