b43a896109aaa1491def5f2984874aa552226605
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-15 22:54:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,5 @@ dia=datetime.timedelta(1)
 
 st.write(os.path.basename('C:\PlenoLed\secrets.toml'))
 
-# define the relative path of the sample file
-file_path = "C:/PlenoLed/secrets.toml"
-
-# store the target API URL
-target_url = "https://httpbin.org/post"
-
-# create a reference to the file
-target_file = open(file_path, "rb")
-
-# send the request
-response = requests.post(target_url, files = {"form_field_name": target_file})
-
-# check the result
-if response.ok:
-    st.write("Upload complete")
-    st.write(response.text)
-else:
-    st.write("Something went wrong")
-
-
-
-
-
 
 
