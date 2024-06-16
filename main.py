229615f957b464c3586744bfb90f25b0e1a48a2e
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-16 00:36:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+from streamlit_file_browser import st_file_browser
 
 arq=st.file_uploader("Escolho",accept_multiple_files=True)
 st.write(pd.read_parquet(arq[0]))
+
+st.header('Default Options')
+event = st_file_browser("example_artifacts", key='A')
+st.write(event)
 
 
 
