3e3bd8b85b8a822914a1de003a2c686be968a719
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-16 00:46:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from streamlit_file_browser import st_file_browser
 arq=st.file_uploader("Escolho",accept_multiple_files=True)
 st.write(pd.read_parquet(arq[0]))
 
-st.header('Default Options')
-event = st_file_browser('')
+
+st.header('Show only molecule files')
+event = st_file_browser("example_artifacts", artifacts_site="http://localhost:1024", show_choose_file=True, show_download_file=False, glob_patterns=('molecule/*',), key='C')
 st.write(event)
 
 
