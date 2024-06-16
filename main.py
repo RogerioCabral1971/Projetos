6f2f97d9243c2f728337747d3ee1328e2c090390
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-15 21:33:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,11 +5,32 @@ import pandas as pd
 import montar_pag as pag
 import atualizar_bases
 import extrair_informacoes as ext
+import tomllib
+import requests
 
 today=datetime.date.today()
 dia=datetime.timedelta(1)
 status = 'Finalizado'
 dir=ext.ler_toml()['pastas']['dir']
+
+# define the relative path of the sample file
+file_path = "C:/PlenoLed/secrets.toml"
+
+# store the target API URL
+target_url = "https://httpbin.org/post"
+
+# create a reference to the file
+target_file = open(file_path, "rb")
+
+# send the request
+response = requests.post(target_url, files = {"form_field_name": target_file})
+
+# check the result
+if response.ok:
+    st.write("Upload complete")
+    st.write(response.text)
+else:
+    st.write("Something went wrong")
 
 # CONFIGURAÇÃO DA PAGINA
 st.set_page_config(layout='wide', page_title='PlenoLed', initial_sidebar_state="expanded")
