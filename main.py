1f57919f098e9d17f9e070719ea04cef0f459deb
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-15 22:57:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 
 today=datetime.date.today()
 dia=datetime.timedelta(1)
-with open(os.path.basename('C:\PlenoLed\secrets.toml'), "rb") as f:
+with open(os.path.basename('C:\PlenoLed\secrets.toml')) as f:
         valores = tomllib.load(f)
 st.write(valore)
 
