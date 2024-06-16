726e253d9047f4184c5a9f433c29f1729c01ded4
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-16 02:01:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
 import tomllib
+import os
 
-
-with open("6136afc4-e52d-4eb5-acb1-2bd35bfb7959", "rb") as f:
-    valores = tomllib.load(f)
-
-
+os.getcwd()
 
 
 
