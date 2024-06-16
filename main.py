51df1497860c06df1d8c6af954c9fd027ccbdd1d
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-06-16 01:08:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_file_browser import st_file_browser
+
 
 arq=st.file_uploader("Escolho",accept_multiple_files=True)
 st.write(pd.read_parquet(arq[0]))
