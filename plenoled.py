df41d192793b023be5f722de0f36a74f12b279ca
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-08-19 21:50:02 -0300

    Update plenoled.py

--- plenoled.py
+++ plenoled.py
@@ -3,12 +3,7 @@ import datetime
 import locale
 import streamlit as st
 import pandas as pd
-import montar_pag as pag
-import atualizar_bases
-import extrair_informacoes as ext
-import relatorio_plenoled as rel
-import baixar_atualização
-import abrirArq
+
 
 # CONFIGURAÇÃO DA PAGINA
 st.set_page_config(layout='wide', page_title='PlenoLed',initial_sidebar_state='collapsed')
